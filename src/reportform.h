d8b8c1585a4f584445c3d9e4ba2b2cac650d68ae
mtonin01 <mtonin01@gmail.com>
2013-05-09 11:46:30 +0000

    - migliorata produzione report

--- src/reportform.h
+++ src/reportform.h
@@ -3,21 +3,25 @@
 
 #include "ui_reportform.h"
 #include <QTextCursor>
-
+#include <QMap>
 class ReportForm : public QWidget, private Ui::ReportForm
 {
   Q_OBJECT
   
 public:
-  explicit ReportForm(QWidget *parent = 0);
+  explicit ReportForm(QMap<QString,QVariant>* par,QWidget *parent = 0);
 private slots:
   void on_stampaBtn_clicked();
 
 private:
-  void stampaTutto(const QString& nomeFile);
-  void stampaPerReparti(const QString& nomeFile);
-  void stampaPerDestinazione(const QString& nomeFile);
+  void stampaTutto();
+  void stampaPerReparti();
+  void stampaPerDestinazione();
+  void creaPdf(const QTextDocument* doc,const QString descrReport);
   void putHeader(QTextCursor cursore, const QString testo);
+
+  QMap<QString,QVariant>* configurazione;
+
 };
 
 #endif // REPORTFORM_H
