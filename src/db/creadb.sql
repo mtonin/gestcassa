2dcdfd6c096b258527f310fd5a3fe740a3e2a19e
mtonin01 <mtonin01@gmail.com>
2013-05-21 07:49:05 +0000

    - aggiunto sql per creazione database

---
+++ src/db/creadb.sql
@@ -0,0 +1,52 @@
+
+-- Table: destinazionistampa
+CREATE TABLE destinazionistampa ( 
+    nome         TEXT PRIMARY KEY,
+    intestazione TEXT 
+);
+
+
+-- Table: articoli
+CREATE TABLE articoli ( 
+    idarticolo   INTEGER PRIMARY KEY AUTOINCREMENT,
+    descrizione  VARCHAR,
+    prezzo       REAL,
+    idreparto    INTEGER,
+    riga         INTEGER,
+    colonna      INTEGER,
+    abilitato    BOOLEAN,
+    destinazione TEXT    REFERENCES destinazionistampa ( nome ),
+    UNIQUE ( idreparto, riga, colonna ) 
+);
+
+
+-- Table: reparti
+CREATE TABLE reparti ( 
+    idreparto    INTEGER         PRIMARY KEY AUTOINCREMENT,
+    descrizione  VARCHAR( 50 )   NOT NULL,
+    font         VARCHAR( 100 ),
+    coloresfondo VARCHAR( 50 ) 
+);
+
+
+-- Table: ordini
+CREATE TABLE ordini ( 
+    numero   INTEGER  PRIMARY KEY,
+    tsstampa DATETIME 
+);
+
+
+-- Table: righeordine
+CREATE TABLE righeordine ( 
+    numeroordine INTEGER REFERENCES ordini ( numero ),
+    idarticolo   INTEGER REFERENCES articoli ( idarticolo ),
+    quantita     INTEGER 
+);
+
+
+-- Table: configurazione
+CREATE TABLE configurazione ( 
+    chiave TEXT PRIMARY KEY,
+    valore TEXT 
+);
+
