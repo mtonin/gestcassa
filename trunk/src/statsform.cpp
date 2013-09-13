926f37ce71ccb54375f14de0100a4312f28e7e7b
mtonin01@gmail.com <mtonin01@gmail.com@7b37eaf2-c9fd-24a7-1397-8e65191c4207>
2013-09-13 12:55:29 +0000

    - aggiunto export in pdf delle statistiche

--- trunk/src/statsform.cpp
+++ trunk/src/statsform.cpp
@@ -1,11 +1,19 @@
 #include "statsform.h"
 #include "qcustomplot.h"
+#include "textprinter.h"
 
 #include <QtSql>
 #include <QMessageBox>
+#include <QTextDocument>
+#include <QTextCursor>
+#include <QTextTable>
+#include <QTextDocumentFragment>
+#include <QTextDocumentWriter>
 
-StatsForm::StatsForm(const int idSessione, QWidget *parent) : idSessioneCorrente(idSessione), QDialog(parent)
+StatsForm::StatsForm(QMap<QString, QVariant> *par, QWidget *parent) : configurazione(par), QDialog(parent)
 {
+  idSessioneCorrente=configurazione->value("sessioneCorrente").toInt();
+
   setupUi(this);
   setWindowFlags(Qt::Dialog|Qt::MSWindowsFixedSizeDialogHint|Qt::CustomizeWindowHint);
   activateWindow();
@@ -58,13 +66,13 @@ void StatsForm::caricaStats()
   QVector<double> tickData;
   int numero=0;
 
-  QString sql("SELECT descrizione,sum(quantita) \
+  QString sql("SELECT descrizione,sum(quantita),tsstampa \
               FROM storicoordini \
               where \
               %1 \
               and tipoArticolo <> ? \
               group by descrizione \
-              order by 2 desc");
+              order by 3 asc");
   QString condizione;
   if(ultimaSessioneBox->isChecked()) {
     condizione="idsessione=?";
@@ -96,7 +104,14 @@ void StatsForm::caricaStats()
     return;
   }
   statsModel->clear();
+  tsInizio.clear();
+  tsFine.clear();
+
   while(stmt.next()) {
+    if(tsInizio.isEmpty()) {
+      tsInizio=stmt.value(2).toDateTime().toString("dd-MM-yyyy hh:mm:ss");
+    }
+    tsFine=stmt.value(2).toDateTime().toString("dd-MM-yyyy hh:mm:ss");
     QString nomeArticolo=stmt.value(0).toString();
     int quantita=stmt.value(1).toInt();
     QList<QStandardItem*> riga;
@@ -178,9 +193,112 @@ void StatsForm::calcolaTotali()
     return;
   }
   stmt.next();
-  int totOrdini=stmt.value(0).toInt();
-  float totImporto=stmt.value(1).toFloat();
+  totOrdini=stmt.value(0).toInt();
+  totImporto=stmt.value(1).toFloat();
 
   totaleOrdiniTxt->setText(QString("%L1").arg(totOrdini));
   totaleImportoTxt->setText(QString("%1 %L2").arg(QChar(0x20AC)).arg(totImporto,4,'f',2));
 }
+
+void StatsForm::on_stampaBtn_clicked(){
+
+  QTextBlockFormat rightAlignFormat;
+  rightAlignFormat.setAlignment(Qt::AlignRight);
+  QTextBlockFormat centerAlignFormat;
+  centerAlignFormat.setAlignment(Qt::AlignCenter);
+
+  QTextDocument* documento=new QTextDocument(this);
+  QTextCursor cursoreDoc(documento);
+  QString testo;
+
+  cursoreDoc.setBlockFormat(centerAlignFormat);
+  cursoreDoc.insertHtml("<center><h2><u>CONDIZIONI</u></h2></center><br>");
+  testo=QString("Nome cassa: %1\n").arg(configurazione->value("nomeCassa").toString());
+  cursoreDoc.insertText(testo);
+  testo=QString("Primo scontrino: %1\n").arg(tsInizio);
+  cursoreDoc.insertText(testo);
+  testo=QString("Ultimo scontrino: %1\n").arg(tsFine);
+  cursoreDoc.insertText(testo);
+  testo=QString("Ordini emessi: %L1\n").arg(totOrdini);
+  cursoreDoc.insertText(testo);
+  testo=QString("Importo incassato: %1 %L2\n\n").arg(QChar(0x20AC)).arg(totImporto,4,'f',2);
+  cursoreDoc.insertText(testo);
+
+  cursoreDoc.setBlockFormat(centerAlignFormat);
+  cursoreDoc.insertHtml("<center><h2><u>TOTALIZZATORI</u></h2></center><br>");
+  testo=QString("Nome cassa: %1\n").arg(configurazione->value("nomeCassa").toString());
+
+  QTextDocument tableDocument;
+  QTextCursor cursore(&tableDocument);
+  QTextTable* tabella=cursore.insertTable(1,2);
+
+  foreach(testo,((QString)"ARTICOLO,QUANTITA'").split(",")) {
+    putHeader(cursore,testo);
+    cursore.movePosition(QTextCursor::NextCell);
+  }
+
+  for(int i=0;i<statsModel->rowCount();i++) {
+    QString descrizioneArt=statsModel->item(i,0)->data(Qt::DisplayRole).toString();
+    QString quantita=statsModel->item(i,1)->data(Qt::DisplayRole).toString();
+    tabella->appendRows(1);
+    cursore.movePosition(QTextCursor::PreviousCell,QTextCursor::MoveAnchor,1);
+    cursore.insertText(descrizioneArt);
+    cursore.movePosition(QTextCursor::NextCell);
+    cursore.setBlockFormat(rightAlignFormat);
+    cursore.insertText(quantita);
+  }
+
+  cursoreDoc.insertFragment(QTextDocumentFragment(&tableDocument));
+  //QString descrReport=QString("Totali di cassa -- CASSA %1 -- &date;").arg(configurazione->value("nomeCassa").toString());
+  QString descrReport=QString("Totali di cassa - &date;");
+  stampa(documento,descrReport,false);
+
+}
+
+void StatsForm::stampa(const QTextDocument *doc, const QString descrReport, bool preview)
+{
+
+  TextPrinter* tprinter=new TextPrinter(this);
+  QString header=QString("<center><b>%1<br>%2</b></center>").arg(configurazione->value("descrManifestazione").toString()).arg(descrReport);
+  tprinter->setHeaderText(header);
+  tprinter->setFooterText("<center><b>GESTIONE CASSA</b><br>Pag. &page;</center>");
+  tprinter->setHeaderSize(5);
+  tprinter->setFooterSize(10);
+  tprinter->setDateFormat("d MMM yyyy h:mm:ss");
+
+  if(preview) {
+    tprinter->preview(doc);
+  } else {
+    tprinter->exportPdf(doc,"Salva con nome");
+  }
+
+}
+
+void StatsForm::putHeader(QTextCursor cursore, const QString testo)
+{
+  QTextBlockFormat format;
+  format.setAlignment(Qt::AlignHCenter);
+  QTextCharFormat charFormat=cursore.charFormat();
+  QFont font=cursore.document()->defaultFont();
+  font.setPointSize(font.pointSize()+2);
+  font.setBold(true);
+  charFormat.setFont(font);
+  cursore.setBlockFormat(format);
+  cursore.setCharFormat(charFormat);
+  cursore.insertText(testo);
+}
+
+void StatsForm::formattaTabella(QTextTable* tabella)
+{
+  QTextTableFormat formatoTabella;
+  formatoTabella.setBorder(1);
+  formatoTabella.setBorderStyle(QTextFrameFormat::BorderStyle_Solid);
+  formatoTabella.setHeaderRowCount(1);
+  formatoTabella.setCellSpacing(-1);
+  formatoTabella.setCellPadding(3);
+  formatoTabella.setWidth(QTextLength(QTextLength::PercentageLength,100));
+
+  tabella->setFormat(formatoTabella);
+
+}
+
