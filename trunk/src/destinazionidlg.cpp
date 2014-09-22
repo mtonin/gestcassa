e5167dce9a153d2771507c0f74a28aae2618bce0
mtonin01@gmail.com <mtonin01@gmail.com@7b37eaf2-c9fd-24a7-1397-8e65191c4207>
2014-09-22 09:45:27 +0000

    - modificata gestione destinazioni di stampa

--- trunk/src/destinazionidlg.cpp
+++ trunk/src/destinazionidlg.cpp
@@ -1,7 +1,9 @@
 #include "destinazionidlg.h"
+#include "confermadlg.h"
 
 #include <QMessageBox>
 #include <QSqlRecord>
+#include <QtSql>
 
 DestinazioniDlg::DestinazioniDlg(QWidget *parent) :
     QWidget(parent)
@@ -11,7 +13,7 @@ DestinazioniDlg::DestinazioniDlg(QWidget *parent) :
     modello = new QSqlTableModel;
     modello->setEditStrategy(QSqlTableModel::OnFieldChange);
     modello->setTable("destinazionistampa");
-    //modello->setSort(0,Qt::AscendingOrder);
+    modello->setSort(0,Qt::AscendingOrder);
     modello->select();
     destinazioneTbl->setModel(modello);
     destinazioneTbl->hideColumn(1);
@@ -56,14 +58,49 @@ void DestinazioniDlg::on_cancellaBtn_clicked()
 
 void DestinazioniDlg::on_nuovoBtn_clicked()
 {
+    ConfermaDlg dlg("Inserire il nome della nuova destinazione.", "Nome", false,this);
+    if (QDialog::Accepted != dlg.visualizza()) return;
+    QString nuovaDestinazione=dlg.getValore();
+
+    QString query("insert into destinazionistampa values (?,'INTESTAZIONE TAGLIANDO',1,0)");
+    QSqlQuery sql;
+    sql.prepare(query);
+    sql.addBindValue(nuovaDestinazione);
+    if(!sql.exec()) {
+      QSqlError errore=sql.lastError();
+      QString msg;
+      if(-803==errore.number()) {
+        msg="Destinazione già presente.";
+      } else {
+        msg=QString("Errore codice=%1,descrizione=%2").arg(errore.number()).arg(errore.text());
+      }
+      QMessageBox::critical(this,"Errore",msg);
+    } else {
+      modello->select();
+    }
+    destinazioneTbl->setCurrentIndex(modello->index(0, 0));
+    destinazioneTxt->setFocus();
+    /*
     int numRighe = modello->rowCount();
     modello->insertRow(numRighe);
-    modello->setData(modello->index(numRighe, 0), "NOME DESTINAZIONE");
+    modello->setData(modello->index(numRighe, 0), nuovaDestinazione);
     modello->setData(modello->index(numRighe, 1), "INTESTAZIONE TAGLIANDO");
     modello->setData(modello->index(numRighe, 2), true);
     modello->setData(modello->index(numRighe, 3), false);
-    modello->submitAll();
-    destinazioneTbl->setCurrentIndex(modello->index(numRighe, 0));
-    destinazioneTxt->selectAll();
-    destinazioneTxt->setFocus();
+    if(!modello->submitAll()) {
+      QSqlError errore=modello->lastError();
+      QString msg;
+      if(-803==errore.number()) {
+        msg="Destinazione già presente.";
+      } else {
+        msg=QString("Errore codice=%1,descrizione=%2").arg(errore.number()).arg(errore.text());
+      }
+      QMessageBox::critical(this,"Errore",msg);
+      modello->removeRow(numRighe);
+    } else {
+      destinazioneTbl->setCurrentIndex(modello->index(numRighe, 0));
+      destinazioneTxt->selectAll();
+      destinazioneTxt->setFocus();
+    }
+    */
 }
