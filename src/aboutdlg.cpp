2be0cbb0d987f50fce6f99f2a96ed7c180d71e82
mtonin01 <mtonin01@gmail.com>
2015-06-17 11:33:55 +0000

    - modificato indirizzo email nell'about

--- src/aboutdlg.cpp
+++ src/aboutdlg.cpp
@@ -3,7 +3,7 @@
 
 const QString ABOUTMSG("<center><b><i>GESTCASSA</i></b><br>versione $VERSIONE<br>"
 "<p>Programma per la gestione delle postazioni di cassa durante una sagra.<br><br>"
-"Per informazioni e licenza d'uso, inviare una mail a <i>mtonin01@gmail.com</i>.</p>");
+"Per informazioni e licenza d'uso, inviare una mail a <i>gestcassa@yahoo.com</i></p>");
 
 AboutDlg::AboutDlg(QWidget *parent) :
 QDialog(parent)
