79db576dbbd0bdc55fd5a914742546f31dca60d6
mauro <mtonin01@gmail.com>
2015-09-08 10:40:53 +0200

    - aggiunto file svnrev.h

---
+++ src/svnrev.h
@@ -0,0 +1,9 @@
+#ifndef SVNREV_H
+#define SVNREV_H
+
+#include <string>
+#include "versione.h"
+
+const std::string SVN_REV(GEN_VER_DIGITAL_VERSION_STRING);
+const std::string VERSIONE(GEN_VER_DIGITAL_VERSION_STRING);
+#endif // SVNREV_H
