cce8126241d0e142bca190c25bfaa05305cff2d1
mauro <mtonin01@gmail.com>
2016-05-16 16:03:52 +0200

    - corretti warnings - corretto flag sulla colonna storno

--- src/storicoordinimodel.cpp
+++ src/storicoordinimodel.cpp
@@ -27,13 +27,13 @@ QVariant storicoOrdiniModel::data(const QModelIndex &index, int role) const
     }
     switch (index.column()) {
     case 0:
-        if (Qt::TextAlignmentRole == role) return Qt::AlignRight | Qt::AlignVCenter;
+        if (Qt::TextAlignmentRole == role) return (QVariant)(Qt::AlignRight | Qt::AlignVCenter);
         break;
     case 2:
-        if (Qt::TextAlignmentRole == role) return Qt::AlignRight | Qt::AlignVCenter;
+        if (Qt::TextAlignmentRole == role) return (QVariant)(Qt::AlignRight | Qt::AlignVCenter);
         break;
     case 3:
-        if (Qt::TextAlignmentRole == role) return Qt::AlignRight | Qt::AlignVCenter;
+        if (Qt::TextAlignmentRole == role) return (QVariant)(Qt::AlignRight | Qt::AlignVCenter);
         break;
     case 4:
         break;
@@ -42,7 +42,7 @@ QVariant storicoOrdiniModel::data(const QModelIndex &index, int role) const
             float importo = rigaArticolo.toFloat();
             return QString("%1 %L2").arg(QChar(0x20AC)).arg(importo, 4, 'f', 2);
         }
-        if (Qt::TextAlignmentRole == role) return Qt::AlignRight | Qt::AlignVCenter;
+        if (Qt::TextAlignmentRole == role) return (QVariant)(Qt::AlignRight | Qt::AlignVCenter);
         break;
     case 6:
         if (Qt::DisplayRole == role) {
@@ -92,7 +92,7 @@ Qt::ItemFlags storicoOrdiniModel::flags(const QModelIndex &index) const
 {
     Qt::ItemFlags flags = QAbstractItemModel::flags(index);
     if (6 == index.column()) {
-        flags |=  Qt::ItemIsUserCheckable | Qt::ItemIsEnabled;
+        flags |=  Qt::ItemIsUserCheckable |Qt::ItemIsEditable;
     }
     return flags;
 }
