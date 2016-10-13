2d228da7842bcba5ba383e02ca03be6cd7f78537
mauro <mtonin01@gmail.com>
2016-10-13 15:59:49 +0200

    - correzione prepare per insert/update reparti

--- src/dettaglireparto.cpp
+++ src/dettaglireparto.cpp
@@ -31,7 +31,12 @@ void DettagliReparto::setCurrentReparto(RepartoBtnWidget *currentRepartoBtn)
 
 void DettagliReparto::aggiornaReparto()
 {
-    QSqlQuery query("update or insert into reparti (idreparto,descrizione,font,coloresfondo,colorecarattere,abilitato,adattafont) values(?,?,?,?,?,?,?)");
+    QSqlQuery query;
+    if(!query.prepare("update or insert into reparti (idreparto,descrizione,font,coloresfondo,colorecarattere,abilitato,adattafont) values(?,?,?,?,?,?,?)")) {
+        MessaggioDlg msgBox("Database Error", query.lastError().text(),this);
+        msgBox.visualizza();
+        return;
+    }
     query.addBindValue(repartoBtn->getId());
     query.addBindValue(testoReparto->text());
     query.addBindValue(repartoBtn->getFont());
@@ -39,8 +44,7 @@ void DettagliReparto::aggiornaReparto()
     query.addBindValue(repartoBtn->getColoreText().toRgb());
     query.addBindValue(repartoBtn->getAbilitato());
     query.addBindValue(repartoBtn->getAdattaFont());
-    query.exec();
-    if (!query.isActive()) {
+    if (!query.exec()) {
         MessaggioDlg msgBox("Database Error", query.lastError().text(),this);
         msgBox.visualizza();
         return;
